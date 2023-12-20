2582012e97ef0e7be5dbc15d9b375f09ca196e31
codefug <robot9917@naver.com>
2023-12-21 08:57:16 +0900

    complete renewal of the project (mk1)

---
+++ script.js
@@ -0,0 +1,4 @@
+const Loading=document.querySelector("#Loading");
+const lastcard=document.querySelector(".lastcard");
+
+window.onload=setTimeout(()=>{Loading.setAttribute("style","display:none")},2000);
